95be987278834e16aeec3a9dfe6d192f406091ca
C-bowman <chris.bowman.physics@gmail.com>
2019-06-21 15:56:47 +0100

    preparation for listing inference-tools on PyPi

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="inference-tools",
+    version="0.4.1",
+    author="Chris Bowman",
+    author_email="chris.bowman.physics@gmail.com",
+    description="A collection of python tools for Bayesian data analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/C-bowman/inference_tools",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
