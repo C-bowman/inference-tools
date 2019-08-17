346797b6d6f5ce73548f23f4e38f2d2296ce76b1
C-bowman <chris.bowman.physics@gmail.com>
2019-08-17 13:33:31 +0100

    Further developmnt of the ParallelTempering demo

--- demos/ParallelTempering_demo.py
+++ demos/ParallelTempering_demo.py
@@ -1,15 +1,21 @@
 
-from numpy import log, sqrt, sin, arctan2
+from numpy import log, sqrt, sin, arctan2, pi
 
+# define a posterior with multiple separate peaks
 def multimodal_posterior(theta):
     x,y = theta
     r = sqrt(x**2 + y**2)
     phi = arctan2(y,x)
-    return -0.5*((r - 1.)/0.1)**2  + 2*log(sin(phi*1.5)**2)
+    z = ((r - (0.5 + pi - phi*0.5))/0.1)
+    return -0.5*z**2  + 4*log(sin(phi*2.)**2)
 
 from inference.mcmc import GibbsChain, ParallelTempering
 
-temps = [2.**k for k in range(6)]
+# define a set of temperature levels
+N_levels = 6
+temps = [10**(2.5*k/(N_levels-1.)) for k in range(N_levels)]
+
+# create a set of chains - one with each temperature
 chains = [ GibbsChain( posterior=multimodal_posterior, start = [0.5,0.5], temperature=T) for T in temps ]
 for c in chains:
     c.print_status = False
@@ -17,16 +23,16 @@ for c in chains:
 
 PT = ParallelTempering(chains=chains)
 
-for i in range(500):
-    PT.advance(50)
+for i in range(2000):
+    PT.advance(20)
     PT.swap()
+
 
 chains = PT.return_chains()
 
 chains[0].plot_diagnostics()
-
 chains[0].trace_plot()
-
 chains[0].matrix_plot()
 
+# trigger the shutdown event to terminate all the processes
 PT.shutdown()
