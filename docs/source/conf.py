4eb1e15bfe9a45e199689c2cf166dd7d9e3e25e8
C-bowman <chris.bowman.physics@gmail.com>
2020-07-18 19:19:53 +0100

    Incremented version to 0.5.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Chris Bowman'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.5.2'
+release = '0.5.4'
 
 
 # -- General configuration ---------------------------------------------------
