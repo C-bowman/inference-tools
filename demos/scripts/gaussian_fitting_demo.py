150d07695b9ed215741240542360f1b4b71fe909
C-bowman <chris.bowman.physics@gmail.com>
2021-06-15 00:56:27 +0100

    Completed prototype of the gaussian fitting demo

--- demos/scripts/gaussian_fitting_demo.py
+++ demos/scripts/gaussian_fitting_demo.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 # Gaussian peak plus a constant background. Our goal in this example is to
 # infer the area of the Gaussian.
 
-x_data = [1.0, 1.6, 2.2, 2.8, 3.4, 4.0, 4.6, 5.2,
-          5.8, 6.4, 7.0, 7.6, 8.2, 8.8, 9.4, 10.]
+x_data = [0.00, 0.80, 1.60, 2.40, 3.20, 4.00, 4.80, 5.60,
+          6.40, 7.20, 8.00, 8.80, 9.60, 10.4, 11.2, 12.0]
 
 y_data = [2.473, 1.329, 2.370, 1.135, 5.861, 7.045, 9.942, 7.335,
           3.329, 5.348, 1.462, 2.476, 3.096, 0.784, 3.342, 1.877]
@@ -50,7 +50,6 @@ class PeakModel(object):
         gaussian = exp(-0.5*z**2)/(sqrt(2*pi)*width)
         return area*gaussian + background
 
-
 # Inference-tools has a variety of Likelihood classes which allow you to easily construct a
 # likelihood function given the measured data and your forward-model.
 from inference.likelihoods import GaussianLikelihood
@@ -71,19 +70,16 @@ print(guess_log_likelihood)
 # prior distributions across all model parameters.
 from inference.priors import ExponentialPrior, UniformPrior, JointPrior
 
-# First we need to make an ordered list of strings which identify all our model parameters.
-model_variables = ['area', 'width', 'center', 'background']
-
 # If we want different model parameters to have different prior distributions, as in this
 # case where we give three variables an exponential prior and one a uniform prior, we first
 # construct each type of prior separately:
 prior_components = [
-    ExponentialPrior(beta=[20., 20., 20.], variables=['area', 'width', 'background']),
-    UniformPrior(lower=1., upper=10., variables=['center'])
+    ExponentialPrior(beta=[50., 20., 20.], variable_indices=[0, 1, 3]),
+    UniformPrior(lower=0., upper=12., variable_indices=[2])
 ]
 # Now we use the JointPrior class to combine the various components into a single prior
 # distribution which covers all the model parameters.
-prior = JointPrior(model_variables=model_variables, components=prior_components)
+prior = JointPrior(components=prior_components, n_variables=4)
 
 # As with the likelihood, prior objects can also be called as function to return a
 # log-probability value when passed a vector of model parameters. We can also draw
@@ -91,42 +87,57 @@ prior = JointPrior(model_variables=model_variables, components=prior_components)
 prior_sample = prior.sample()
 print(prior_sample)
 
-class Posterior(object):
-    def __init__(self, likelihood=None, prior=None):
-        self.likelihood = likelihood
-        self.prior = prior
-
-    def __call__(self, theta):
-        return self.likelihood(theta) + self.prior(theta)
+# The likelihood and prior can be easily combined into a posterior distribution
+# using the Posterior class:
+from inference.posterior import Posterior
 posterior = Posterior(likelihood=likelihood, prior=prior)
 
+# Now we have constructed a posterior distribution, we can sample from it
+# using Markov-chain Monte-Carlo (MCMC).
 
+# The inference.mcmc module contains implementations of various MCMC sampling algorithms.
+# Here we import the PcaChain class and use it to create a Markov-chain object:
+from inference.mcmc import PcaChain
+chain = PcaChain(posterior=posterior, start=initial_guess)
 
-
-
-from inference.mcmc import GibbsChain
-chain = GibbsChain(posterior=posterior, start=[5., 5., 5., 5.])
+# We generate samples by advancing the chain by a chosen number of steps using the advance method:
 chain.advance(25000)
 
-chain.burn = 5000
-chain.thin = 2
-
+# we can check the status of the chain using the plot_diagnostics method:
 chain.plot_diagnostics()
 
-chain.matrix_plot(labels=model_variables)
+# The burn-in (how many samples from the start of the chain are discarded)
+# can be chosen by setting the burn attribute of the chain object:
+chain.burn = 5000
+
+# we can get a quick overview of the posterior using the matrix_plot method
+# of chain objects, which plots all possible 1D & 2D marginal distributions
+# of the full parameter set (or a chosen sub-set).
+chain.matrix_plot(labels=['area', 'width', 'center', 'background'])
+
+# We can easily estimate 1D marginal distributions for any parameter
+# using the get_marginal method:
+area_pdf = chain.get_marginal(0)
+area_pdf.plot_summary(label='Gaussian area')
+
+
+# We can assess the level of uncertainty in the model predictions by passing each sample
+# through the forward-model and observing the distribution of model expressions that result:
 
 # generate an axis on which to evaluate the model
-x_fits = linspace(1, 10, 500)
+x_fits = linspace(0, 12, 500)
 # get the sample
 sample = chain.get_sample()
 # pass each through the forward model
 curves = array([PeakModel.forward_model(x_fits, theta) for theta in sample])
 
-plt.figure(figsize=(8, 5))
+# We could plot the predictions for each sample all on a single graph, but this is
+# often cluttered and difficult to interpret.
 
-# We can use the hdi_plot function from the plotting module to plot
+# A better option is to use the hdi_plot function from the plotting module to plot
 # highest-density intervals for each point where the model is evaluated:
 from inference.plotting import hdi_plot
+plt.figure(figsize=(8, 5))
 hdi_plot(x_fits, curves, intervals=[0.68, 0.95])
 
 # plot the MAP estimate (the sample with the single highest posterior probability)
