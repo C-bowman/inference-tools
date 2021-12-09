4d9fc06113cacf886a684a3356cd9f2a35f301b5
C-bowman <chris.bowman.physics@gmail.com>
2021-12-09 01:43:48 +0000

    Improved testing for covariance functions

--- tests/test_covariance.py
+++ tests/test_covariance.py
@@ -1,73 +1,53 @@
-from numpy import linspace, sin, eye
+import pytest
+from numpy import linspace, sin, isfinite
 from numpy.random import normal, seed
-from inference.gp import SquaredExponential, RationalQuadratic
+from inference.gp import SquaredExponential, RationalQuadratic, WhiteNoise
 
 
-def test_SquaredExponential_covariance_and_gradients():
+def covar_findiff(cov_func=None, x0=None, delta=1e-5):
+    grad = []
+    for i in range(x0.size):
+        x1 = x0.copy()
+        x2 = x0.copy()
+        dx = x0[i] * delta
+
+        x1[i] -= dx
+        x2[i] += dx
+
+        f1 = cov_func.covariance_and_gradients(x1)[0]
+        f2 = cov_func.covariance_and_gradients(x2)[0]
+        grad.append(0.5 * (f2 - f1) / dx)
+    return grad
+
+
+def create_data():
     seed(2)
     N = 20
     S = 0.1
-    x = linspace(0, 10, N).reshape([N, 1])
+    x = linspace(0, 10, N)
     y = sin(x) + 0.1 + normal(size=N) * S
+    return x.reshape([N,1]), y
 
-    cov = SquaredExponential()
+
+@pytest.mark.parametrize(
+    "cov",
+    [
+        SquaredExponential(),
+        RationalQuadratic(),
+        WhiteNoise(),
+        RationalQuadratic() + WhiteNoise()
+    ]
+)
+def test_covariance_and_gradients(cov):
+    x, y = create_data()
     cov.pass_data(x, y)
+    theta = normal(size=cov.n_params) + 0.5
+    _, grads = cov.covariance_and_gradients(theta)
+    fd_grads = covar_findiff(cov_func=cov, x0=theta)
 
-    A = 0.1
-    L = 0.6
-    delta = 1e-5
-    _, grads = cov.covariance_and_gradients([A, L])
-    K_A_pos, _ = cov.covariance_and_gradients([A * (1 + delta), L])
-    K_A_neg, _ = cov.covariance_and_gradients([A * (1 - delta), L])
-    K_L_pos, _ = cov.covariance_and_gradients([A, L * (1 + delta)])
-    K_L_neg, _ = cov.covariance_and_gradients([A, L * (1 - delta)])
-
-    grad_A, grad_L = grads
-
-    fd_grad_A = (K_A_pos - K_A_neg) / (2 * A * delta)
-    fd_grad_L = (K_L_pos - K_L_neg) / (2 * L * delta)
-
-    D = 1e-50 * eye(N)
-    A_max_fractional_error = abs(fd_grad_A / grad_A - 1).max()
-    L_max_fractional_error = abs((fd_grad_L + D) / (grad_L + D) - 1).max()
-
-    assert A_max_fractional_error < 1e-6
-    assert L_max_fractional_error < 1e-6
-
-
-def test_RationalQuadratic_covariance_and_gradients():
-    seed(2)
-    N = 20
-    S = 0.1
-    x = linspace(0, 10, N).reshape([N, 1])
-    y = sin(x) + 0.1 + normal(size=N) * S
-
-    cov = RationalQuadratic()
-    cov.pass_data(x, y)
-
-    A = 0.1
-    q = 1.2
-    L = 0.6
-    delta = 1e-5
-    _, grads = cov.covariance_and_gradients([A, q, L])
-    grad_A, grad_q, grad_L = grads
-
-    K_A_pos, _ = cov.covariance_and_gradients([A * (1 + delta), q, L])
-    K_A_neg, _ = cov.covariance_and_gradients([A * (1 - delta), q, L])
-    K_q_pos, _ = cov.covariance_and_gradients([A, q * (1 + delta), L])
-    K_q_neg, _ = cov.covariance_and_gradients([A, q * (1 - delta), L])
-    K_L_pos, _ = cov.covariance_and_gradients([A, q, L * (1 + delta)])
-    K_L_neg, _ = cov.covariance_and_gradients([A, q, L * (1 - delta)])
-
-    fd_grad_A = (K_A_pos - K_A_neg) / (2 * A * delta)
-    fd_grad_q = (K_q_pos - K_q_neg) / (2 * q * delta)
-    fd_grad_L = (K_L_pos - K_L_neg) / (2 * L * delta)
-
-    D = 1e-50 * eye(N)
-    A_max_fractional_error = abs(fd_grad_A / grad_A - 1).max()
-    q_max_fractional_error = abs((fd_grad_q + D) / (grad_q + D) - 1).max()
-    L_max_fractional_error = abs((fd_grad_L + D) / (grad_L + D) - 1).max()
-
-    assert A_max_fractional_error < 1e-6
-    assert q_max_fractional_error < 1e-6
-    assert L_max_fractional_error < 1e-6
+    for ang, fdg in zip(grads, fd_grads):
+        zero_inds = (ang == 0.) & (fdg == 0.)
+        abs_frac_err = abs(fdg / ang - 1.)
+        abs_frac_err[zero_inds] = 0.
+        assert isfinite(abs_frac_err).all()
+        assert abs_frac_err.max() < 1e-6
