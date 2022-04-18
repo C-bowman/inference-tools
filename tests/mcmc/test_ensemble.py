16a5391a102a305496322281bb1b33f5835bf393
C-bowman <chris.bowman.physics@gmail.com>
2022-04-19 00:15:24 +0100

    Added a save/load test for EnsembleSampler

--- tests/mcmc/test_ensemble.py
+++ tests/mcmc/test_ensemble.py
@@ -20,7 +20,7 @@ def test_ensemble_sampler_advance(line_posterior):
 
     n_iterations = 25
     chain.advance(iterations=n_iterations)
-    chain.n_iterations = n_iterations
+    assert chain.n_iterations == n_iterations
 
     sample = chain.get_sample()
     assert sample.shape == (n_iterations * n_walkers, 2)
@@ -30,4 +30,29 @@ def test_ensemble_sampler_advance(line_posterior):
 
     probs = chain.get_probabilities()
     assert probs.shape == (n_iterations * n_walkers,)
-    chain.plot_diagnostics()
+
+
+def test_ensemble_sampler_restore(line_posterior, tmp_path):
+    n_walkers = 100
+    guess = array([2.0, -4.0])
+    rng = default_rng(256)
+    starts = [guess * rng.normal(scale=0.01, loc=1., size=2) for i in range(n_walkers)]
+
+    chain = EnsembleSampler(
+        posterior=line_posterior,
+        starting_positions=starts
+    )
+
+    n_iterations = 25
+    chain.advance(iterations=n_iterations)
+
+    filename = tmp_path / "restore_file.npz"
+    chain.save(filename)
+
+    new_chain = EnsembleSampler.load(filename)
+
+    assert new_chain.n_iterations == chain.n_iterations
+    assert (new_chain.theta == chain.theta).all()
+    assert (new_chain.probs == chain.probs).all()
+    assert (new_chain.sample == chain.sample).all()
+    assert (new_chain.sample_probs == chain.sample_probs).all()
