369ba74ccc5f938582b1d7b020aa30ec6a624d2e
C-bowman <chris.bowman.physics@gmail.com>
2023-07-03 10:25:51 +0100

    Refactored conditional distribution sampling code

--- inference/approx/conditional.py
+++ inference/approx/conditional.py
@@ -1,4 +1,5 @@
-from numpy import array, ndarray, insert, exp, linspace, zeros
+from numpy import exp, sqrt, insert, searchsorted
+from numpy import array, ndarray, linspace, zeros
 from numpy.random import default_rng
 from scipy.integrate import simps
 
@@ -31,6 +32,51 @@ def binary_search(func: callable, target: float, limits: list, tol=0.05) -> floa
     return x3
 
 
+def trapezium_full(x, dh):
+    b = dh - 1
+    return (b + sqrt(b**2 + 4 * x * dh)) / (2 * dh)
+
+
+def trapezium_near_zero(x, dh):
+    return x + (1 - x) * x * dh
+
+
+def trapezium_transform(x: ndarray, dh: ndarray) -> ndarray:
+    """
+    Transforms uniformly distributed random numbers in the interval [0, 1]
+    to trapezium-distributed numbers in the interval [0, 1].
+    
+    :param x: \
+        The random numbers to be transformed as a numpy ``ndarray``.
+    
+    :param dh: \
+        The difference between the uniform distribution and the trapezium
+        distribution at x = 1.
+    """
+    near_zero = abs(dh) < 1e-5
+    if near_zero.any():
+        stable = ~near_zero
+        t = zeros(x.size)
+        t[near_zero] = trapezium_near_zero(x[near_zero], dh[near_zero])
+        t[stable] = trapezium_full(x[stable], dh[stable])
+        return t
+    else:
+        return trapezium_full(x, dh)
+
+
+def piecewise_linear_sample(x: ndarray, y: ndarray, n_samples: int) -> ndarray:
+    dx = x[1:] - x[:-1]
+    means = 0.5 * (y[1:] + y[:-1])
+    delta = 0.5 * (y[1:] - y[:-1]) / means
+    weights = means / dx
+    weights /= weights.sum()
+    # first sample indices of the trapeziums based on their total probability
+    inds = rng.choice(weights.size, size=n_samples, p=weights)
+    # now sample from trapezium distributions
+    trapz = trapezium_transform(rng.random(size=n_samples), delta[inds]) * dx[inds]
+    return x[inds] + trapz
+
+
 def evaluate_conditional(func: callable, points: ndarray, grid_size=64):
     p = array([func(x) for x in points])
     x = points.copy()
@@ -57,8 +103,64 @@ def evaluate_conditional(func: callable, points: ndarray, grid_size=64):
     return x_cond, p_cond
 
 
+def get_conditionals(
+    posterior: callable, bounds: list, conditioning_point: ndarray, grid_size: int = 64
+):
+    """
+    Evaluates each of the 1D conditional distributions of the posterior around a given
+    point in the parameter space. For each conditional, the space within the given
+    bounds is searched to determine the range of values containing non-negligible
+    probability, and the conditional is evaluated on a uniform grid covering that range.
+
+    :param posterior: \
+        A function which returns the posterior log-probability when given a
+        numpy ``ndarray`` of the model parameters.
+
+    :param bounds: \
+        A list of length-2 tuples specifying the lower and upper bounds on
+        each parameter, in the form ``(lower, upper)``.
+
+    :param conditioning_point: \
+        The point in the parameter space around which the conditional distributions are
+        evaluated.
+
+    :param grid_size: \
+        The number of points used to evaluate each of the conditional distributions.
+
+    :return samples: \
+        The samples as a 2D numpy ``ndarray`` which has shape
+        ``(n_samples, n_parameters)``.
+    """
+    conditional = Conditional(
+        posterior=posterior, theta=conditioning_point, variable_index=0
+    )
+
+    n_params = conditioning_point.size
+    n_search_points = 16
+
+    axes = zeros([grid_size, n_params])
+    prob = zeros([grid_size, n_params])
+    for i in range(n_params):
+        # switch to the conditional for the current variable
+        conditional.variable_index = i
+        # search using evenly spaced points plus the value from the mode
+        search_points = linspace(*bounds[i], n_search_points)
+        if (search_points != conditioning_point[i]).all():
+            index = searchsorted(search_points, conditioning_point[i])
+            search_points = insert(search_points, index, conditioning_point[i])
+
+        # evaluate the conditional where its probability is non-negligible
+        x_cond, p_cond = evaluate_conditional(
+            func=conditional, points=search_points, grid_size=grid_size
+        )
+
+        axes[:, i] = x_cond
+        prob[:, i] = p_cond
+    return axes, prob
+
+
 def conditional_sample(
-    posterior: callable, bounds: list, mode: ndarray, n_samples: int
+    posterior: callable, bounds: list, conditioning_point: ndarray, n_samples: int
 ) -> ndarray:
     """
     Generates samples from each of the 1D conditional distributions of the posterior
@@ -71,12 +173,13 @@ def conditional_sample(
         numpy ``ndarray`` of the model parameters.
 
     :param bounds: \
-        A list of length-2 tuples specifying the lower and upper bounds to be set on
-        each parameter, in the form (lower, upper).
+        A list of length-2 tuples specifying the lower and upper bounds on
+        each parameter, in the form ``(lower, upper)``.
 
-    :param mode: \
-        The parameters used for the conditioning - ideally these should correspond to
-        the mode / MAP estimate.
+    :param conditioning_point: \
+        The point in the parameter space around which the conditional distributions are
+        evaluated. This point should correspond the posterior mode if the conditional
+        distribution samples are to be used as approximate posterior samples.
 
     :param n_samples: \
         Number of samples to draw.
@@ -85,24 +188,12 @@ def conditional_sample(
         The samples as a 2D numpy ``ndarray`` which has shape
         ``(n_samples, n_parameters)``.
     """
-    conditional = Conditional(posterior=posterior, theta=mode, variable_index=0)
+    axes, probs = get_conditionals(
+        posterior=posterior, bounds=bounds, conditioning_point=conditioning_point
+    )
 
-    n_params = mode.size
-    assert len(bounds) == n_params
-
-    n_search_points = 64
+    grid_size, n_params = probs.shape
     samples = zeros([n_samples, n_params])
     for i in range(n_params):
-        conditional.variable_index = i
-        search_points = linspace(*bounds[i], n_search_points)
-        x_cond, p_cond = evaluate_conditional(func=conditional, points=search_points)
-
-        weights = p_cond / p_cond.sum()
-        dx = x_cond[1] - x_cond[0]
-        # As the grid is evenly spaced, we can first sample the indices of the grid-points,
-        # then add samples from a triangle distribution with a half-width equal
-        # to the grid-spacing. This is equivalent to sampling from a piecewise-linear
-        # approximation of the conditional PDF.
-        samples[:, i] = x_cond[rng.choice(p_cond.size, size=n_samples, p=weights)]
-        samples[:, i] += rng.triangular(left=-dx, mode=0.0, right=dx, size=n_samples)
+        samples[:, i] = piecewise_linear_sample(axes[:, i], probs[:, i], n_samples)
     return samples
