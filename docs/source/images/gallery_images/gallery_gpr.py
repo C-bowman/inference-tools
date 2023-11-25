c097aec9e8de76f9533bf6f6ce6994291801c1d7
C-bowman <chris.bowman.physics@gmail.com>
2023-11-25 22:23:59 +0000

    Updates to gallery images used in README

--- docs/source/images/gallery_images/gallery_gpr.py
+++ docs/source/images/gallery_images/gallery_gpr.py
@@ -1,58 +1,45 @@
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from numpy import exp, sin, sqrt
-from numpy import linspace, zeros, array, meshgrid
-from numpy.random import multivariate_normal as mvn
-from numpy.random import normal, random, seed
-from inference.gp import GpRegressor
-
-seed(3) #4
-
-"""
-Code demonstrating the use of the GpRegressor class found in inference.gp_tools
-"""
-
-# create some testing data
-Nx = 9*2
-x = list( linspace(-3,1,Nx//2) )
-x.extend( list( linspace(4,9,Nx//2) ) )
-x = array(x)
-
-# generate points q at which to evaluate the
-# GP regression estimate
-Nq = 200
-q = linspace(-4, 10, Nq) # cover whole range, including the gap
-
-
-sig = 0.1 # assumed normal error on the data points
-y_c = ( 1. / (1 + exp(-q)) ) + 0.1*sin(2*q) # underlying function
-y   = ( 1. / (1 + exp(-x)) ) + 0.1*sin(2*x) + sig*normal(size=len(x)) # sampled y data
-errs = zeros(len(y)) + sig # y data errors
+from numpy import linspace, array
+from inference.gp import GpRegressor, SquaredExponential
 
 
 # initialise the class with the data and errors
-GP = GpRegressor(x, y, y_err = errs)
-
-# call the instance to get estimates for the points in q
-mu_q, sig_q = GP(q)
+x_fit = linspace(0, 5, 200)
+x = array([0.5, 1.0, 1.5, 3.0, 3.5, 4.0, 4.5])
+y = array([0.157, -0.150, -0.305, -0.049, 0.366, 0.417, 0.430]) * 10.0
+y_errors = array([0.1, 0.01, 0.1, 0.4, 0.1, 0.01, 0.1]) * 10.0
+gpr = GpRegressor(x, y, y_err=y_errors, kernel=SquaredExponential())
+mu, sig = gpr(x_fit)
 
 # now plot the regression estimate and the data together
-c1 = 'red'; c2 = 'blue'; c3 = 'green'
-fig = plt.figure( figsize = (5,4) )
+col = "blue"
+fig = plt.figure(figsize=(6, 5))
 ax = fig.add_subplot(111)
-ax.plot(q, mu_q, lw = 2, color = c2, label = 'posterior mean')
-ax.fill_between(q, mu_q-sig_q, mu_q-sig_q*2, color = c2, alpha = 0.15, label = r'$\pm 2 \sigma$ interval')
-ax.fill_between(q, mu_q+sig_q, mu_q+sig_q*2, color = c2, alpha = 0.15)
-ax.fill_between(q, mu_q-sig_q, mu_q+sig_q, color = c2, alpha = 0.4, label = r'$\pm 1 \sigma$ interval')
-# ax.plot(x, y, 'o', color = c1, label = 'data', markerfacecolor = 'none', markeredgewidth = 2)
-ax.plot(x, y, 'o', color = c1, label = 'data', marker = 'D', ls = 'none', markeredgecolor = 'black')
-ax.set_ylim([-0.1, 1.3])
-ax.set_xlim([-4, 10])
-# ax.set_title('Prediction using posterior mean and covariance', fontsize = 10)
-ax.set_ylabel('function value', fontsize = 1)
-ax.set_xlabel('spatial coordinate', fontsize = 10)
+ax.fill_between(
+    x_fit, mu - sig, mu + sig, color=col, alpha=0.2, label="GPR uncertainty"
+)
+ax.fill_between(x_fit, mu - 2 * sig, mu + 2 * sig, color=col, alpha=0.1)
+ax.plot(x_fit, mu, lw=2, c=col, label="GPR mean")
+ax.errorbar(
+    x,
+    y,
+    yerr=y_errors,
+    marker="o",
+    color="black",
+    ecolor="black",
+    ls="none",
+    label="data values",
+    markerfacecolor="none",
+    markeredgewidth=2,
+    markersize=10,
+    elinewidth=2
+)
+ax.set_xlim([0, 5])
+ax.set_ylim([-7, 7])
+ax.set_xlabel("x-data value", fontsize=11)
+ax.set_ylabel("y-data value", fontsize=11)
 ax.grid()
-ax.legend(loc=4, fontsize = 10)
+ax.legend(loc=4, fontsize=12)
 plt.tight_layout()
-plt.savefig('gallery_gpr.png')
+plt.savefig("gallery_gpr.png")
 plt.show()
