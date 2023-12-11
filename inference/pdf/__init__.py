8c719ecb9e14f54829e58720b0e3b3d6c979ac88
C-bowman <C-bowman@users.noreply.github.com>
2023-12-11 16:16:36 +0000

    [skip ci] Apply black changes

--- inference/pdf/__init__.py
+++ inference/pdf/__init__.py
@@ -3,11 +3,4 @@ from inference.pdf.kde import GaussianKDE, KDE2D
 from inference.pdf.unimodal import UnimodalPdf
 from inference.pdf.hdi import sample_hdi
 
-__all__ = [
-    "DensityEstimator",
-    "GaussianKDE",
-    "KDE2D",
-    "UnimodalPdf",
-    "sample_hdi"
-]
-
+__all__ = ["DensityEstimator", "GaussianKDE", "KDE2D", "UnimodalPdf", "sample_hdi"]
