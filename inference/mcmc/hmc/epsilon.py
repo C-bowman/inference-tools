3938ae1a0d6910af8bf22a8376400cce2b561705
C-bowman <chris.bowman.physics@gmail.com>
2026-02-14 15:49:46 +0000

    Introduced new algorithm for tuning HMC step-size

--- inference/mcmc/hmc/epsilon.py
+++ inference/mcmc/hmc/epsilon.py
@@ -1,68 +1,160 @@
+from collections import defaultdict
 from copy import copy
-from numpy import sqrt, log
+from numpy import sqrt, log, exp, array
+
+from scipy.special import ndtr as normal_cdf
+
+
+class RunningStats:
+    def __init__(self):
+        self.mean: float = 0.0
+        self.S: float = 0.0
+        self.variance: float = 0.0
+        self.count: int = 0
+        self.update = self.first_sample
+
+    def first_sample(self, x):
+        self.count = 1
+        self.mean = x
+        self.update = self.add_sample
+
+    def add_sample(self, x):
+        self.count += 1
+        mu = self.mean + (x - self.mean) / self.count
+        self.S += (x - self.mean) * (x - mu)
+        self.mean = mu
+        self.variance = self.S / (self.count - 1)
 
 
 class EpsilonSelector:
-    def __init__(self, epsilon: float):
-        # storage
-        self.epsilon = epsilon
-        self.epsilon_values = [copy(epsilon)]  # sigma values after each assessment
-        self.epsilon_checks = [0.0]  # chain locations at which sigma was assessed
+    def __init__(self, initial_epsilon: float):
+        # The spacing of the discretisation of the log-epsilon value
+        self.ln_eps_spacing = log(1.02)
+        # the factor by which epsilon is increased or decreased when searching
+        self.search_factor = 2.0
+        # the number of indices to jump in order to achieve the desired search factor
+        self.index_jump = int(round(log(self.search_factor) / self.ln_eps_spacing))
+        # round the given epsilon initial guess to the nearest discretised value
+        self.current_index = round(log(initial_epsilon) / self.ln_eps_spacing)
+        self.epsilon = exp(self.ln_eps_spacing * self.current_index)
 
-        # tracking variables
-        self.avg = 0
-        self.var = 0
-        self.num = 0
+        # storage to record history of changes in epsilon
+        self.epsilon_values = [copy(self.epsilon)]
+        self.epsilon_updates = [0]
 
-        # settings for epsilon adjustment algorithm
+        # tracking of mean / variance of acceptance rate at each epsilon
+        self.stats = defaultdict(RunningStats)
+        self.counter = 0
+        self.total_proposals = 0
+
+        # target acceptance rate
         self.accept_rate = 0.65
-        self.chk_int = 15  # interval of steps at which proposal widths are adjusted
-        self.growth_factor = 1.4  # growth factor for self.chk_int
+        # interval of steps at which proposal widths are adjusted
+        self.update_interval = 25
+        self.min_var = 1e-3
 
     def add_probability(self, p: float):
-        self.num += 1
-        self.avg += p
-        self.var += max(p * (1 - p), 0.03)
+        self.stats[self.current_index].update(p)
+        self.counter += 1
 
-        if self.num >= self.chk_int:
+        if self.counter >= self.update_interval:
+            self.total_proposals += self.counter
+            self.counter = 0
             self.update_epsilon()
 
+            if self.epsilon != self.epsilon_values[-1]:
+                self.epsilon_values.append(self.epsilon)
+                self.epsilon_updates.append(self.total_proposals)
+
     def update_epsilon(self):
-        """
-        looks at the acceptance rate of proposed steps and adjusts the epsilon
-        value to bring the acceptance rate toward its target value.
-        """
-        # normal approximation of poisson binomial distribution
-        mu = self.avg / self.num
-        std = sqrt(self.var) / self.num
+        bin_inds = array([i for i in self.stats.keys()])
+        bin_probs = array(
+            [self.below_target_probability(s) for s in self.stats.values()]
+        )
 
-        # now check if the desired success rate is within 2-sigma
-        if ~(mu - 2 * std < self.accept_rate < mu + 2 * std):
-            adj = (log(self.accept_rate) / log(mu)) ** 0.15
-            adj = min(adj, 2.0)
-            adj = max(adj, 0.5)
-            self.adjust_epsilon(adj)
-        else:  # increase the check interval
-            self.chk_int = int((self.growth_factor * self.chk_int) * 0.1) * 10
+        sorter = bin_inds.argsort()
+        bin_inds = bin_inds[sorter]
+        bin_probs = bin_probs[sorter]
+        bin_probs = bin_probs.clip(1e-4, 1 - 1e-4)
 
-    def adjust_epsilon(self, ratio: float):
-        self.epsilon *= ratio
-        self.epsilon_values.append(copy(self.epsilon))
-        self.epsilon_checks.append(self.epsilon_checks[-1] + self.num)
-        self.avg = 0
-        self.var = 0
-        self.num = 0
+        interval_probs = [bin_probs.prod()]
+        for p in bin_probs:
+            new = interval_probs[-1] * (1 - p) / p
+            interval_probs.append(new)
+
+        interval_probs = array(interval_probs)
+        max_ind = interval_probs.argmax()
+
+        if max_ind == 0:
+            self.current_index = bin_inds[0] - self.index_jump
+        elif max_ind == interval_probs.size - 1:
+            self.current_index = bin_inds[-1] + self.index_jump
+        else:
+            # get the indices of the two bin edges
+            lwr = bin_inds[max_ind - 1]
+            upr = bin_inds[max_ind]
+
+            # if the bins are adjacent, we calculate the one with the highest
+            # likelihood of being equal to the target rate
+            if upr - lwr == 1:
+                lwr_prob = self.target_likelihood(self.stats[lwr])
+                upr_prob = self.target_likelihood(self.stats[upr])
+                self.current_index = upr if upr_prob >= lwr_prob else lwr
+
+            # otherwise, aim for a new bin in the middle of the two
+            else:
+                self.current_index = int(round((lwr + upr) * 0.5))
+
+        # calculate new epsilon based on new index
+        self.epsilon = exp(self.ln_eps_spacing * self.current_index)
+
+    def below_target_probability(self, stats: RunningStats) -> float:
+        """
+        Calculates the probability that the true acceptance rate for the epsilon value
+        corresponding to the given `RunningStats` instance is below the target
+        acceptance rate by assuming a gaussian uncertainty on the mean of the observed
+        acceptance rate
+        """
+        inv_sigma = sqrt(stats.count / max(stats.variance, self.min_var))
+        z = (self.accept_rate - stats.mean) * inv_sigma
+        return normal_cdf(z)
+
+    def target_likelihood(self, stats: RunningStats) -> float:
+        inv_sigma = sqrt(stats.count / max(stats.variance, self.min_var))
+        z = (self.accept_rate - stats.mean) * inv_sigma
+        return exp(-0.5 * z**2) * inv_sigma
 
     def get_items(self):
-        return self.__dict__
+        items = self.__dict__.copy()
+        items.pop("stats")
+
+        items["stats_mean"] = [rs.mean for rs in self.stats.values()]
+        items["stats_variance"] = [rs.variance for rs in self.stats.values()]
+        items["stats_s"] = [rs.S for rs in self.stats.values()]
+        items["stats_counts"] = [rs.count for rs in self.stats.values()]
+        items["stats_index"] = [i for i in self.stats.keys()]
+        return items
 
     def load_items(self, dictionary: dict):
         self.epsilon = float(dictionary["epsilon"])
         self.epsilon_values = list(dictionary["epsilon_values"])
-        self.epsilon_checks = list(dictionary["epsilon_checks"])
-        self.avg = float(dictionary["avg"])
-        self.var = float(dictionary["var"])
-        self.num = float(dictionary["num"])
+        self.epsilon_updates = list(dictionary["epsilon_updates"])
         self.accept_rate = float(dictionary["accept_rate"])
-        self.chk_int = int(dictionary["chk_int"])
-        self.growth_factor = float(dictionary["growth_factor"])
+        self.update_interval = int(dictionary["update_interval"])
+        self.counter = int(dictionary["counter"])
+        self.total_proposals = int(dictionary["total_proposals"])
+        self.ln_eps_spacing = float(dictionary["ln_eps_spacing"])
+        self.search_factor = float(dictionary["search_factor"])
+        self.current_index = int(dictionary["current_index"])
+
+        # rebuild the dictionary of RunningStats objects
+        self.stats = {}
+        for k, index in enumerate(dictionary["stats_index"]):
+            rs = RunningStats()
+            rs.mean = dictionary["stats_mean"][k]
+            rs.variance = dictionary["stats_variance"][k]
+            rs.s = dictionary["stats_s"][k]
+            rs.count = int(dictionary["stats_counts"][k])
+            if rs.count > 0:
+                rs.update = rs.add_sample
+            self.stats[index] = rs
